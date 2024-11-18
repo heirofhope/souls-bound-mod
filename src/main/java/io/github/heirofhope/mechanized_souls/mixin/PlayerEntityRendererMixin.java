7f79027d892d30232152061a80596246e4380c5f
heirofhope <heirofhope3016@gmail.com>
2024-11-17 21:34:20 -0500

    commented out non-function part of ModConfiguredFeatures initial commit of PlayerEntityRendererMixin added PlayerEntityRendererMixin to mechanized_souls.mixins.json

---
+++ src/main/java/io/github/heirofhope/mechanized_souls/mixin/PlayerEntityRendererMixin.java
@@ -0,0 +1,9 @@
+package io.github.heirofhope.mechanized_souls.mixin;
+
+
+import net.minecraft.client.render.entity.PlayerEntityRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(PlayerEntityRenderer.class)
+public class PlayerEntityRendererMixin {
+}
