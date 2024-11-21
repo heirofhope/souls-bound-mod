96ec1da8012c74fa923621a07980668cd9f6597f
foxracoon <julien_owen@outlook.com>
2024-11-21 09:31:09 -0800

    registers open and closed contracts as writablebookitem and written book item respecfuly

--- src/main/java/io/github/heirofhope/mechanized_souls/item/ModItems.java
+++ src/main/java/io/github/heirofhope/mechanized_souls/item/ModItems.java
@@ -1,9 +1,7 @@
 package io.github.heirofhope.mechanized_souls.item;
 
 import io.github.heirofhope.mechanized_souls.MechanizedSouls;
-import net.minecraft.item.FoodComponent;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
@@ -20,9 +18,9 @@ public class ModItems {
 	public static final Item book_cypher = registeritem("book_cypher",
 		new Item(new QuiltItemSettings().group(ItemGroup.MISC)));
 	public static final Item closed_contract = registeritem("closed_contract",
-		new Item(new QuiltItemSettings().group(ItemGroup.MISC)));
+		new WrittenBookItem (new QuiltItemSettings().group(ItemGroup.MISC)));
 	public static final Item open_contract = registeritem("open_contract",
-		new Item(new QuiltItemSettings().group(ItemGroup.MISC)));
+		new WritableBookItem (new QuiltItemSettings().group(ItemGroup.MISC)));
 
 
 
