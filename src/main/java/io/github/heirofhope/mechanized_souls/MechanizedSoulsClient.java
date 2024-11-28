323657e16df959e723eec7c8863b95159253d0f5
Hour <andrija.pusic2009@gmail.com>
2024-11-28 19:04:43 +0100

    Ferrocium Knight with Geckolib, but animations are still broken so DO NOT TOUCH THE knigth FILES!

--- src/main/java/io/github/heirofhope/mechanized_souls/MechanizedSoulsClient.java
+++ src/main/java/io/github/heirofhope/mechanized_souls/MechanizedSoulsClient.java
@@ -1,10 +1,7 @@
 package io.github.heirofhope.mechanized_souls;
 
 import io.github.heirofhope.mechanized_souls.entity.ModEntities;
-import io.github.heirofhope.mechanized_souls.entity.client.KnightModel;
 import io.github.heirofhope.mechanized_souls.entity.client.KnightRenderer;
-import io.github.heirofhope.mechanized_souls.entity.client.ModModelLayers;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
@@ -14,7 +11,6 @@ public class MechanizedSoulsClient implements ClientModInitializer {
 	public void onInitializeClient(ModContainer mod) {
 
 		EntityRendererRegistry.register(ModEntities.KNIGHT, KnightRenderer::new);
-		EntityModelLayerRegistry.registerModelLayer(ModModelLayers.KNIGHT, KnightModel::getTexturedModelData);
 
 	}
 }
--- src/main/java/io/github/heirofhope/mechanized_souls/entity/client/KnightRenderer.java
+++ src/main/java/io/github/heirofhope/mechanized_souls/entity/client/KnightRenderer.java
@@ -1,30 +1,20 @@
 package io.github.heirofhope.mechanized_souls.entity.client;
 
 
-import io.github.heirofhope.mechanized_souls.entity.custom.KnightEntity;
 import io.github.heirofhope.mechanized_souls.MechanizedSouls;
-import net.minecraft.client.render.VertexConsumerProvider;
+import io.github.heirofhope.mechanized_souls.entity.custom.KnightEntity;
 import net.minecraft.client.render.entity.EntityRendererFactory;
-import net.minecraft.client.render.entity.MobEntityRenderer;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
-public class KnightRenderer extends MobEntityRenderer<KnightEntity,KnightModel<KnightEntity>> {
-	private static final Identifier TEXTURE = new Identifier(MechanizedSouls.MOD_ID, "textures/entity/knight.png");
+public class KnightRenderer extends GeoEntityRenderer<KnightEntity> {
 
-
-	public KnightRenderer(EntityRendererFactory.Context context) {
-		super(context, new KnightModel<>(context.getPart(ModModelLayers.KNIGHT)), .9f);
+	public KnightRenderer(EntityRendererFactory.Context renderManager) {
+		super(renderManager, new KnightModel());
 	}
 
 	@Override
-	public Identifier getTexture(KnightEntity entity) {
-		return TEXTURE;
-	}
-
-	@Override
-	public void render(KnightEntity mobEntity, float f, float g, MatrixStack matrixStack,
-					   VertexConsumerProvider vertexConsumerProvider, int i) {
-		super.render(mobEntity, f, g, matrixStack, vertexConsumerProvider, i);
+	public Identifier getTextureLocation(KnightEntity animatable) {
+		return new Identifier(MechanizedSouls.MOD_ID,"textures/entity/knight/fer_golem.png");
 	}
 }
