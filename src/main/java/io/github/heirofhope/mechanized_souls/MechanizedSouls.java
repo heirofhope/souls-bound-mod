3017f9f3ec12c7eaf1a64a6cdbe5972fdb5c0f0a
feprogramador <100803140+feprogramador@users.noreply.github.com>
2024-11-28 20:57:24 -0300

    Generating string test
    from 100 100 100 to player

--- src/main/java/io/github/heirofhope/mechanized_souls/MechanizedSouls.java
+++ src/main/java/io/github/heirofhope/mechanized_souls/MechanizedSouls.java
@@ -4,29 +4,40 @@ import io.github.heirofhope.mechanized_souls.effect.ModEffects;
 import io.github.heirofhope.mechanized_souls.entity.ModEntities;
 import io.github.heirofhope.mechanized_souls.entity.custom.KnightEntity;
 import io.github.heirofhope.mechanized_souls.item.ModItems;
+import io.github.heirofhope.mechanized_souls.util.ScytheStringRenderer;
 import io.github.heirofhope.mechanized_souls.util.BeforeDeathHandler;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
+import net.minecraft.particle.DefaultParticleType;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 
 public class MechanizedSouls implements ModInitializer {
 	public static final String MOD_ID = "mechanized_souls";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	// Declare SOUL_LINK_PARTICLE at the class level
+	public static final DefaultParticleType SOUL_LINK_PARTICLE = Registry.register(
+		Registries.PARTICLE_TYPE,
+		new Identifier(MOD_ID, "soul_link_particle"),
+		FabricParticleTypes.simple()
+	);
 
+	@Override
+	public void onInitialize(ModContainer mod) {
+		LOGGER.info("Hello Quilt world from {}! Stay fresh!", mod.metadata().name());
 
-    @Override
-    public void onInitialize(ModContainer mod) {
-
-	//	ModConfiguredFeatures.registerConfiguredFeature();
-        LOGGER.info("Hello Quilt world from {}! Stay fresh!", mod.metadata().name());
 		ModItems.RegisterModItems();
 		FabricDefaultAttributeRegistry.register(ModEntities.KNIGHT, KnightEntity.createKnightAttributes());
+		ScytheStringRenderer.register();
 		ModEffects.registerEffects();
 		BeforeDeathHandler.register();
-
-    }
+	}
 }
+
