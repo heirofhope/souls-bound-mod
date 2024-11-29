5ef105ffff4964a2597a8d58d31d15b98c12bdb4
feprogramador <100803140+feprogramador@users.noreply.github.com>
2024-11-28 21:09:23 -0300

    fixing thingiess
    removing my failed attempt to add a particle

--- src/main/java/io/github/heirofhope/mechanized_souls/MechanizedSoulsClient.java
+++ src/main/java/io/github/heirofhope/mechanized_souls/MechanizedSoulsClient.java
@@ -6,20 +6,15 @@ import io.github.heirofhope.mechanized_souls.entity.client.KnightRenderer;
 import io.github.heirofhope.mechanized_souls.entity.client.ModModelLayers;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
-import net.minecraft.client.particle.EndRodParticle;
 import org.quiltmc.loader.api.ModContainer;
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 
 public class MechanizedSoulsClient implements ClientModInitializer {
-
 	@Override
 	public void onInitializeClient(ModContainer mod) {
-		// Register the entity renderer and model layer
+
 		EntityRendererRegistry.register(ModEntities.KNIGHT, KnightRenderer::new);
 		EntityModelLayerRegistry.registerModelLayer(ModModelLayers.KNIGHT, KnightModel::getTexturedModelData);
 
-		// Register the particle factory
-		ParticleFactoryRegistry.getInstance().register(FabricDocsReference.SOUL_LINK_PARTICLE, EndRodParticle.Factory::new);
 	}
 }
