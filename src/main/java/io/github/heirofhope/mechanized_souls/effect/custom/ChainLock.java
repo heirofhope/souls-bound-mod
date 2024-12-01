ee4453c93ca87c86ac89313c7ca89cfe784a2d6a
feprogramador <100803140+feprogramador@users.noreply.github.com>
2024-11-30 22:35:19 -0300

    chain lock effect

--- src/main/java/io/github/heirofhope/mechanized_souls/effect/custom/ChainLock.java
+++ src/main/java/io/github/heirofhope/mechanized_souls/effect/custom/ChainLock.java
@@ -19,19 +19,40 @@ public class ChainLock extends StatusEffect {
 	@Override
 	public void applyUpdateEffect(LivingEntity entity, int amplifier) {
 		if (!entity.getWorld().isClient) {
-
+			// Get entity's current position
 			double Xpos = entity.getX();
 			double Ypos = entity.getY();
 			double Zpos = entity.getZ();
-			BlockPos TargetPos = new BlockPos(100, 100, 100);
-			double Xcorrection = (Math.abs(TargetPos.getX() - Xpos) - 10) * ( (TargetPos.getX() - Xpos) / Math.abs(TargetPos.getX() - Xpos));
-			double Ycorrection = TargetPos.getY() - Ypos;
-			double Zcorrection = TargetPos.getZ() - Zpos;
 
-			entity.teleport(Xpos + Xcorrection,Ypos,Zpos);
-			//entity.teleport(Xpos + Xcorrection,Ypos + Ycorrection,Zpos + Zcorrection);
+			// Define target position
+			BlockPos targetPos = new BlockPos(100, 100, 100);
+			double targetX = targetPos.getX() + 0.5; // Center of the block
+			double targetY = targetPos.getY() + 0.5;
+			double targetZ = targetPos.getZ() + 0.5;
 
-		//	entity.setVelocity(Xcorrection,Ycorrection,Zcorrection);
+			// Calculate distance
+			double deltaX = targetX - Xpos;
+			double deltaY = targetY - Ypos;
+			double deltaZ = targetZ - Zpos;
+
+			double distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY + deltaZ * deltaZ);
+
+			if (distance == 0) {
+				return;
+			}
+
+			double horizontalAngle = Math.atan2(deltaZ, deltaX); // Rotation on XZ plane
+			double verticalAngle = Math.asin(deltaY / distance); // Elevation angle
+
+			double correctionFactor = distance - 10; // Limit movement to 10 units per tick
+			double xCorrection = Math.cos(horizontalAngle) * correctionFactor;
+			double yCorrection = Math.sin(verticalAngle) * correctionFactor;
+			double zCorrection = Math.sin(horizontalAngle) * correctionFactor;
+
+			// Teleport entity with corrections
+			if (distance > 10) {
+				entity.teleport(Xpos + xCorrection, Ypos + yCorrection, Zpos + zCorrection);
+			}
 		}
 		super.applyUpdateEffect(entity, amplifier);
 	}
