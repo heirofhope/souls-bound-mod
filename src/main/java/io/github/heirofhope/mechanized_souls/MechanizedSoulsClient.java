fd5fc3d61d9277e108b7fe18dcc4d874a32e94c4
foxracoon <julien_owen@outlook.com>
2024-12-07 09:34:27 -0800

    hope this fixes

--- src/main/java/io/github/heirofhope/mechanized_souls/MechanizedSoulsClient.java
+++ src/main/java/io/github/heirofhope/mechanized_souls/MechanizedSoulsClient.java
@@ -3,18 +3,15 @@ package io.github.heirofhope.mechanized_souls;
 import io.github.heirofhope.mechanized_souls.entity.ModEntities;
 import io.github.heirofhope.mechanized_souls.entity.client.KnightRenderer;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.render.entity.FlyingItemEntityRenderer;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
 
 public class MechanizedSoulsClient implements ClientModInitializer {
-
-
 	@Override
 	public void onInitializeClient(ModContainer mod) {
 
 		EntityRendererRegistry.register(ModEntities.KNIGHT, KnightRenderer::new);
-		//DO NOT FUCKING TOUCH THIS IF YOU ARENT 100% SURE YOU ARE NOT GONNA FUCK EVRYTHING UP
-		// AND LEAVE IT TO HOUR TO SORT OUT IN THE MIDDLE OF THE NIGHT, Please.
-		// -Hour
 	}
+
 }
