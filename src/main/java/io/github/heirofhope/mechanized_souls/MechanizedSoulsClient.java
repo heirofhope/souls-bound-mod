c6282a0847dd041c479a031bdd06df0fda00a211
heirofhope <heirofhope3016@gmail.com>
2024-12-07 14:07:14 -0500

    created new branch with ALL CURRENT FEATURES. will soon be merged with MAIN

--- src/main/java/io/github/heirofhope/mechanized_souls/MechanizedSoulsClient.java
+++ src/main/java/io/github/heirofhope/mechanized_souls/MechanizedSoulsClient.java
@@ -3,7 +3,7 @@ package io.github.heirofhope.mechanized_souls;
 import io.github.heirofhope.mechanized_souls.entity.ModEntities;
 import io.github.heirofhope.mechanized_souls.entity.client.KnightRenderer;
 
-import io.github.heirofhope.mechanized_souls.entity.client.ModModelLayers;
+//import io.github.heirofhope.mechanized_souls.entity.client.ModModelLayers;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.minecraft.client.particle.RainSplashParticle;
