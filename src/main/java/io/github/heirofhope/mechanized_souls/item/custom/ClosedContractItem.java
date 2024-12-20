ae59638b2a3a7246fcd53c64a725895be4016ae9
Aristu <blazegamer2009@gmail.com>
2024-12-19 21:29:36 -0500

    Almost Finished Knight

--- src/main/java/io/github/heirofhope/mechanized_souls/item/custom/ClosedContractItem.java
+++ src/main/java/io/github/heirofhope/mechanized_souls/item/custom/ClosedContractItem.java
@@ -1,15 +1,20 @@
 package io.github.heirofhope.mechanized_souls.item.custom;
 
+import com.mojang.brigadier.ParseResults;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.WrittenBookItem;
 import net.minecraft.nbt.NbtCompound;
+import net.minecraft.server.BannedPlayerEntry;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.server.network.ServerPlayerInteractionManager;
 import net.minecraft.text.Text;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.Vec2f;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class ClosedContractItem extends WrittenBookItem {
@@ -30,7 +35,7 @@ public class ClosedContractItem extends WrittenBookItem {
 	public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
 		if (!world.isClient && user instanceof ServerPlayerEntity serverPlayer) {
 			// Ban the player
-			serverPlayer.networkHandler.disconnect(Text.literal("You Have Broken Your Contract."));
+			serverPlayer.networkHandler.disconnect(Text.literal("You have been banned for signing this contract."));
 
 			// Remove the item from the player's hand
 			ItemStack stackInHand = user.getStackInHand(hand);
@@ -41,11 +46,8 @@ public class ClosedContractItem extends WrittenBookItem {
 
 			// Log the action
 			String playerName = serverPlayer.getGameProfile().getName();
-			System.out.println("Player " + playerName + " Was Banned For Breaking Their Contract.");
+			System.out.println("Player " + playerName + " was banned for using the ClosedContractItem.");
 		}
 		return TypedActionResult.success(user.getStackInHand(hand));
 	}
 }
-
-
-
